fa4d5e0485e47cc7d41ddff9887822b37be6bdc7
Dinesh5544 <gembali.dinesh.s86@kalvium.community>
2025-05-23 20:45:03 +0530

    post api used

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
+// backend/server.js
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
-
 const groupRoutes = require('./routes/groups');
 
 app.use(cors());
@@ -10,6 +9,7 @@ app.use(express.json());
 
 app.use('/api/groups', groupRoutes);
 
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
