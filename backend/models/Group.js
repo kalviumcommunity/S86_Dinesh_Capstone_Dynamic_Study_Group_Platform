71b6bd1082d21e11bf2a8ad9f67a7e23aabedecc
Dinesh5544 <gembali.dinesh.s86@kalvium.community>
2025-06-26 14:29:51 +0530

    Implemented relationship done

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -1,9 +1,19 @@
-// backend/models/Group.js
 const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  members: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true
+  },
+  members: {
+    type: Number,
+    default: 0
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 });
 
 module.exports = mongoose.model('Group', groupSchema);
